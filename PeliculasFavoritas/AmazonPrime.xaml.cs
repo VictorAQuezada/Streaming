dd0cbab818779e66ee9bfaa87de71ea1cecbc50c
VictorAQuezada <victorquezada220404@gmail.com>
2025-03-13 19:50:14 -0400

    Add files via upload

---
+++ PeliculasFavoritas/AmazonPrime.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PeliculasFavoritas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AmazonPrime : ContentPage
+    {
+        public AmazonPrime()
+        {
+            InitializeComponent();
+        }
+    }
+}
